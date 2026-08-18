602714df3f59ee2fd53cd51c018bb3c4bdbe0c12
Jan Winkler <jan@golithe.dev>
2026-08-18 16:41:20 +0200

    home: move nvim, lazygit, bazelisk, awscli and nix tooling to nix

--- home.nix
+++ home.nix
@@ -1,15 +1,36 @@
 { pkgs, ... }:
 {
-  home.username = "jwi";
-  home.homeDirectory = "/home/jwi";
-  home.stateVersion = "26.05";
-
+  home = {
+    username = "jwi";
+    homeDirectory = "/home/jwi";
+    stateVersion = "26.05";
+  };
   programs.home-manager.enable = true;
 
+  # cli only, nothing that needs graphics (CUDA stays handled by popos)
   home.packages = with pkgs; [
+    # shell
     btop
     ripgrep
     fzf
     fd
+
+    # editor + git
+    neovim
+    lazygit
+
+    # nix tooling
+    statix
+    nixfmt
+
+    # build + cloud
+    bazelisk
+    awscli2
+
+    # nixpkgs ships the launcher as `bazelisk` therefore an alias.
+    (pkgs.runCommand "bazel-as-bazelisk" { } ''
+      mkdir -p $out/bin
+      ln -s ${pkgs.bazelisk}/bin/bazelisk $out/bin/bazel
+    '')
   ];
 }
